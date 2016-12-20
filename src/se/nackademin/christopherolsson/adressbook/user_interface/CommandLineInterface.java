103540eb41891a66ce52e29f236668a61277b395
Fredrik Grimmenhag <fredrik@grimmenhag.se>
2016-12-20 11:35:21 +0100

    CommandLineInterface, GitIgnore certain files.

---
+++ src/se/nackademin/christopherolsson/adressbook/user_interface/CommandLineInterface.java
@@ -0,0 +1,12 @@
+package se.nackademin.christopherolsson.adressbook.user_interface;
+
+/**
+ * Created by Fredrik Grimmenhag on 2016-12-20.
+ */
+public class CommandLineInterface implements InputHandler {
+
+    @Override
+    public void handle(CommandLine commandline) {
+        
+    }
+}
