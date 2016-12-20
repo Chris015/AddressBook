9739c67785663d9bcb23021708d518c4d01f2c97
Fredrik Grimmenhag <fredrik@grimmenhag.se>
2016-12-20 11:29:04 +0100

    CommandLine, Console, Console printer
    CommandLine class
    Console Class
    ConsolePrinter Class

---
+++ src/se/nackademin/christopherolsson/adressbook/user_interface/ConsolePrinter.java
@@ -0,0 +1,10 @@
+package se.nackademin.christopherolsson.adressbook.user_interface;
+
+/**
+ * Created by Fredrik Grimmenhag on 2016-12-20.
+ */
+public interface ConsolePrinter {
+
+    void print(String inputLine);
+
+}
