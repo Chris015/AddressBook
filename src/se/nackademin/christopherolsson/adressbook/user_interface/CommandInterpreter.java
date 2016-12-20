5918c0daed778e25ba5367cfb4953ed2558521bf
Christopher <christopher.olsson@hyperisland.se>
2016-12-20 12:51:08 +0100

    Created CommandInterpreter

---
+++ src/se/nackademin/christopherolsson/adressbook/user_interface/CommandInterpreter.java
@@ -0,0 +1,12 @@
+package se.nackademin.christopherolsson.adressbook.user_interface;
+
+/**
+ * @author Christopher Olsson on 2016-12-20.
+ */
+public class CommandInterpreter {
+
+    public void interpret(CommandLine commandLine) {
+
+    }
+
+}
