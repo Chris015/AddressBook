13a2807ccad76988f2e2c3ddc72fa3449c7dd859
Fredrik Grimmenhag <fredrik@grimmenhag.se>
2016-12-20 12:34:59 +0100

    InputHandler

--- src/se/nackademin/christopherolsson/adressbook/user_interface/InputHandler.java
+++ src/se/nackademin/christopherolsson/adressbook/user_interface/InputHandler.java
@@ -4,5 +4,6 @@ package se.nackademin.christopherolsson.adressbook.user_interface;
  * Created by Fredrik Grimmenhag on 2016-12-20.
  */
 public interface InputHandler {
+
     void handle(CommandLine commandline);
 }
