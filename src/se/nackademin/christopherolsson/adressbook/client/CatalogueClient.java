f07c954318b3de1de8ef88d29dfa35ba98091f00
GK-LAPTOP\Robin Gk <kallrobin92@gmail.com>
2016-12-20 14:47:46 +0100

    Client Initialized

---
+++ src/se/nackademin/christopherolsson/adressbook/client/CatalogueClient.java
@@ -0,0 +1,62 @@
+package se.nackademin.christopherolsson.adressbook.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * Created by Robin Gk on 2016-12-20 as a school project.
+ * email kallrobin92@gmail.com
+ */
+public class CatalogueClient {
+
+    private String host;
+    private int port;
+    private Socket clientSocket;
+    private PrintWriter writer;
+    private BufferedReader reader;
+
+    public CatalogueClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public synchronized void connect() {
+        try {
+            this.clientSocket = new Socket(host, port);
+            writer = new PrintWriter(clientSocket.getOutputStream());
+            reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendRequest(String request) {
+        writer.write(request);
+    }
+
+    public String waitForResponse() throws IOException {
+        String response = null;
+        for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+            if (line.equals("")) {
+                break;
+            }
+            response += line + "\n";
+        }
+        return response;
+    }
+
+    public void diconnect() {
+        try {
+            this.clientSocket.close();
+            this.reader.close();
+            this.writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
---
+++ src/se/nackademin/christopherolsson/adressbook/client/CatalogueLoader.java
@@ -0,0 +1,19 @@
+package se.nackademin.christopherolsson.adressbook.client;
+
+import se.nackademin.christopherolsson.adressbook.registry.remote_registry.RemoteRegistry;
+
+/**
+ * Created by Robin Gk on 2016-12-20 as a school project.
+ * email kallrobin92@gmail.com
+ */
+public class CatalogueLoader {
+
+    RemoteRegistry remoteRegistry;
+
+    public void run(){
+
+        remoteRegistry.add();
+
+    }
+
+}
