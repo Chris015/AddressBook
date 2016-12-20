2fec54f2b114242803768bb252456df74aff963a
Fredrik Grimmenhag <fredrik@grimmenhag.se>
2016-12-20 11:23:41 +0100

    InputHandle, CommandLine Created
    Created InputHandler and CommandLine.

---
+++ src/se/nackademin/christopherolsson/adressbook/user_interface/CommandLine.java
@@ -0,0 +1,11 @@
+package se.nackademin.christopherolsson.adressbook.user_interface;
+
+import java.util.List;
+
+/**
+ * Created by Fredrik Grimmenhag on 2016-12-20.
+ */
+public class CommandLine {
+    String command;
+    List<String> parameters;
+}
---
+++ src/se/nackademin/christopherolsson/adressbook/user_interface/InputHandler.java
@@ -0,0 +1,8 @@
+package se.nackademin.christopherolsson.adressbook.user_interface;
+
+/**
+ * Created by Fredrik Grimmenhag on 2016-12-20.
+ */
+public interface InputHandler {
+    void handle(CommandLine commandline);
+}
